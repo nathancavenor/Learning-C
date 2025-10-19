9a22ba630eab80b45f7fea78eb76b34b506ac2a1
nathancavenor <nathancavenor@icloud.com>
2025-10-19 13:20:26 +0100

    Added all poker functions
    Added functions - did not determine high card or tiebreakers yet.

--- Exercises/functions.c
+++ Exercises/functions.c
@@ -19,6 +19,28 @@ int is_flush(Hand *h){
     return 1;
 }
 
+int is_straight(Hand *h){
+    int counts[15] = {0};
+    int i;
+
+    for (i=0; i<5; i++){
+        int rank = h->cards[i].rank;
+        counts[rank]++;
+        if (counts[rank] >1){
+            return 0;
+        }
+    }
+    for (i = 2; i< 11; i++){
+        if (counts[i] && counts[i+1] && counts[i+2] && counts[i+3] && counts[i+4]){
+            return 1;
+        }
+    }
+    if (counts[14] && counts[2] && counts[3] && counts[4] && counts[5]){
+        return 1;
+    }
+    return 0;
+}
+
 int is_royal_flush(Hand *h){
     if (!is_flush(h))
         return 0;
@@ -44,13 +66,44 @@ int is_royal_flush(Hand *h){
 int is_straight_flush(Hand *h){
     if (!is_flush(h))
         return 0;
-    return 1;
+    return is_straight(h);
 }
 
+int n_of_a_kind(Hand *h){
+    int counts[15] = {0};
+    for (int i = 0; i< 5; i++) counts[h->cards[i].rank]++;
+    int max_1 = 0;
+    int max_2 = 0;
+    for (int i = 2; i<15; i++){
+        if (counts[i] > max_1){
+            max_2 = max_1;
+            max_1 = counts[i];
+        } else if (counts[i] > max_2) {
+            max_2 = counts[i];
+        }
+    }
+    if (max_1 == 4){return 8;}
+    if (max_1 == 3 && max_2 == 2){return 7;}
+    if (max_1 == 3 && max_2 != 2){return 4;}
+    if (max_1 == 2 && max_2 == 2){return 3;}
+    if (max_1 ==2 && max_2 != 2){return 2;}
+    return 0;
+}
+
+
 int hand_rank(Hand *h){
-    if (is_royal_flush(h)) return 10;
-    if (is_flush(h)) return 6;
-    return 1;
+    if (is_flush(h)){
+        if (is_royal_flush(h)){
+            return 10;
+        }
+        return 6;
+    } else if (is_straight_flush(h)){
+        return 9;
+    }
+    if (is_straight(h)){
+        return 5;
+    }
+    return n_of_a_kind(h);
 }
 
 int main(){
